fc5bbf388345d6afb0ec4594d8cdd9058bb51c3e
Marta <marta.mieczynska@gmail.com>
2023-01-31 19:46:31 +0100

    New research files

--- research_and_results/02c_anomaly_detection_overall_percentage.py
+++ research_and_results/02c_anomaly_detection_overall_percentage.py
@@ -34,10 +34,10 @@ from utils.miscellaneous import count_number, Corruption
 
 # ----------------------------!!! EDIT HERE !!! ---------------------------------  
 np.random.seed(1)  # For reproducibility
-filename = 'Gibraltar.h5' # 'Gdansk', 'Baltic', 'Gibraltar'
+filename = 'Baltic.h5' # 'Gdansk', 'Baltic', 'Gibraltar'
 distance = 'euclidean'
 clustering_algorithm = 'DBSCAN'  # 'kmeans' or 'DBSCAN'
-ad_algorithm = 'xgboost' # 'rf' or 'xgboost'
+ad_algorithm = 'rf' # 'rf' or 'xgboost'
 # --------------------------------------------------------------------------------
 
 # Decide what to do
